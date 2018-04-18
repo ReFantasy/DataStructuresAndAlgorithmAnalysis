8062d4f8f0bec2993587e39343680acd5165ecfb
ReFantasy <309741309@qq.com>
2018-04-18 12:25:37 +0800

    update

--- Ch01/intcell.h
+++ Ch01/intcell.h
@@ -1,6 +1,7 @@
 #ifndef __INTCELL_H__
 #define __INTCELL_H__
-
+#include <vector>
+using namespace std;
 class IntCell
 {
 public:
@@ -13,5 +14,42 @@ public:
 private:
 	int _storedValue;
 
+};
+
+template <typename Object>
+class Matrix
+{
+public:
+	Matrix(int rows, int cols)
+		:arr(rows)
+	{
+		for (int i = 0; i < rows; i++)
+			arr[i].resize(cols);
+	}
+
+	const vector<Object>& operator[](size_t i)const
+	{
+		return arr[i];
+	}
+
+	vector<Object>& operator[](size_t i)
+	{
+		return arr[i];
+	}
+
+	int numrows()const
+	{
+		return arr.size();
+	}
+		
+
+	int numcols()const
+	{
+		return numrows() ? arr[0].size() : 0;
+	}
+		
+
+private:
+	vector<vector<Object>> arr;
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
+#include <vector>
 #include "Ch01/intcell.h"
 using namespace std;
 
-// test IntCell
-void TestIntCell();
-
 int main()
 {
-	TestIntCell();
-	return 0;
+	
+ 	return 0;
 }
 
-void TestIntCell()
-{
-	IntCell intcell;
-	intcell.Write(12);
-	cout << intcell.Read() << endl;
-}
+
 
